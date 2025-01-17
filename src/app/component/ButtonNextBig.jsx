cbaa7a918bc3d5d3914c1a14456a905856bef1e5
24LP2879 24LP2879 <24LP2879@G304-CC0235.local>
2025-01-17 12:22:47 +0800

    added onclick

--- src/app/component/ButtonNextBig.jsx
+++ src/app/component/ButtonNextBig.jsx
@@ -2,11 +2,11 @@ import { ChevronRight } from "lucide-react";
 
 export default function ButtonNextBig({ buttonText, handleClick }) {
   return (
-    <div className="w-[416px] h-[44px] p-[10px 12px] rounded-[6px] bg-[#121316] flex justify-center items-center cursor-pointer">
-      <button
-        onClick={handleClick}
-        className="font-Inter font-500 text-4 leading-[24px] text-white"
-      >
+    <div
+      onClick={handleClick}
+      className="w-[416px] h-[44px] p-[10px 12px] rounded-[6px] bg-[#121316] flex justify-center items-center cursor-pointer"
+    >
+      <button className="font-Inter font-500 text-4 leading-[24px] text-white">
         {buttonText}
       </button>
     </div>
